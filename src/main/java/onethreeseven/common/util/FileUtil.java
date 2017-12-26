34e81847b15def2c31340e695d4098f5877a77d7
Luke <llbermingham@gmail.com>
2017-12-26 21:56:44 +1000

    Added #getExtension to FileUtil.java

--- src/main/java/onethreeseven/common/util/FileUtil.java
+++ src/main/java/onethreeseven/common/util/FileUtil.java
@@ -189,4 +189,27 @@ public final class FileUtil {
         return file != null && file.exists() && file.canRead() && !file.isDirectory();
     }
 
+    /**
+     * Get the extension of a file.
+     * @param file The whose extension we want.
+     * @return The extension without the dot.
+     */
+    public static String getExtension(File file){
+        String fileName = file.getName();
+        char ch;
+        int len;
+        if((len = fileName.length())==0 ||
+                (ch = fileName.charAt(len-1))=='/' || ch=='\\' || //in the case of a directory
+                ch=='.' ) //in the case of . or ..
+            return "";
+        int dotInd = fileName.lastIndexOf('.'),
+                sepInd = Math.max(fileName.lastIndexOf('/'), fileName.lastIndexOf('\\'));
+        if( dotInd<=sepInd )
+            return "";
+        else
+            return fileName.substring(dotInd+1).toLowerCase();
+
+
+    }
+
 }
